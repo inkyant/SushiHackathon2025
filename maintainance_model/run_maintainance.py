7257e46d885aede42051e0b3e0236e38eaff1f99
Kamran Hussain <kamran.hssn05@gmail.com>
2025-10-01 16:29:40 -0700

    llm stuff

--- maintainance_model/run_maintainance.py
+++ maintainance_model/run_maintainance.py
@@ -11,10 +11,10 @@ model.load_state_dict(torch.load("working_model.pt", weights_only=True))
 
 def IsEngineFine(engine_stats):
     ''' 1 if the engine is fine. Input array of Engine rpm, Lube oil pressure, Fuel pressure, Coolant pressure, lube oil temp, Coolant temp'''
-    return round(sig(model(torch.tensor(engine_stats))).item())
+    return "Engine is fine ✅" if round(sig(model(torch.tensor(engine_stats))).item()) == 1 else "MAINTAINANCE REQUIRED ❌"
 
 if __name__ == "__main__":
-    # Engine rpm  Lub oil pressure  Fuel pressure  Coolant pressure  lub oil temp  Coolant temp
+    # Engine, rpm, Lub oil pressure, Fuel pressure, Coolant pressure, lub oil temp, Coolant temp
     d = [791.23, 3.30, 4.65, 2.33, 77.64, 78.42]
     print(sig(model(torch.tensor(d))))
 
